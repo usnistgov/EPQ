5577b610a1c83dba01cf0563c4a853af2f58fba1
Nicholas Ritchie <nritchie@duck-and-cover.com>
2026-07-14 11:39:09 -0400

    Addressing bug in Bruker Esprit 1.9

--- src/gov/nist/microanalysis/EPQTools/BrukerTXT.java
+++ src/gov/nist/microanalysis/EPQTools/BrukerTXT.java
@@ -6,6 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
 
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -13,6 +17,7 @@ import gov.nist.microanalysis.EPQLibrary.BaseSpectrum;
 import gov.nist.microanalysis.EPQLibrary.EPQException;
 import gov.nist.microanalysis.EPQLibrary.SpectrumProperties;
 import gov.nist.microanalysis.EPQLibrary.SpectrumUtils;
+import gov.nist.microanalysis.EPQLibrary.Detector.XRayWindowFactory;
 
 /**
  * <p>
@@ -37,6 +42,13 @@ public class BrukerTXT extends BaseSpectrum {
 
    private final SpectrumProperties mProperties = new SpectrumProperties();
    private double[] mData;
+   
+   static private final List<String> BREAK_LIST = Arrays.asList(new String[] {
+         "Energy Counts",
+         "Energy  Counts",
+         "Energie Impulse",
+         "Energie  Impulse"
+      });
 
    public static boolean isInstanceOf(InputStream is) {
       final InputStreamReader isr = new InputStreamReader(is);
@@ -56,20 +68,32 @@ public class BrukerTXT extends BaseSpectrum {
    public BrukerTXT(String filename) throws FileNotFoundException, XmlPullParserException, IOException, EPQException {
       this(new FileInputStream(filename));
    }
-
+   
+   private boolean isChannelsBreak(String str) {
+      return BREAK_LIST.stream().anyMatch(s->str.startsWith(s));
+   }
+   
    public BrukerTXT(InputStream is) throws IOException, EPQException {
       final InputStreamReader isr = new InputStreamReader(is);
       final BufferedReader br = new BufferedReader(isr);
+      
       String first = br.readLine();
       if ((first == null) || !first.equals("Bruker Nano GmbH Berlin, Germany"))
          throw new EPQException("This spectrum does not appear to be a Bruker TXT file spectrum.");
       String second = br.readLine();
       if ((second == null) || !second.startsWith("Esprit"))
          throw new EPQException("This spectrum does not appear to be a Bruker TXT file spectrum.");
-      for (String str = br.readLine(); (str != null) && (!str.startsWith("Energy Counts")); str = br.readLine()) {
-         str = str.trim();
-         if (str.startsWith("Date: ")) {
-            // Ignore
+      for (var st = br.readLine(); (st != null) && (!isChannelsBreak(st)); st = br.readLine()) {
+         var str = st.trim();
+         if (str.startsWith("Date:")) {
+            // 7/12/2026 11:10:14 AM (Maybe US only ordering/format???)
+            var df = new SimpleDateFormat("M/d/y K:m:s a");
+            try {
+               var date = df.parse(str.substring("Date: ".length()).trim());
+               mProperties.setTimestampProperty(SpectrumProperties.AcquisitionTime, date);
+            } catch (ParseException e) {
+               System.err.println("Unable to parse \"" + str + "\" as a date.");
+            }
          } else if (str.startsWith("Real time:")) {
             double val = Double.parseDouble(str.substring("Real time:".length()).trim());
             mProperties.setNumericProperty(SpectrumProperties.RealTime, val / 1000.0);
@@ -97,9 +121,10 @@ public class BrukerTXT extends BaseSpectrum {
             String typ = str.substring("Detector type:".length());
             mProperties.setTextProperty(SpectrumProperties.DetectorDescription, typ);
          } else if (str.startsWith("Window type:")) {
-            // String typ = str.substring("Window type:".length());
-            // mProperties.setTextProperty(SpectrumProperties.DetectorWindow,
-            // typ);
+            String typ = str.substring("Window type:".length());
+            if (typ.endsWith("AP3.3")) {
+               mProperties.setObjectProperty(SpectrumProperties.DetectorWindow, XRayWindowFactory.createWindow(XRayWindowFactory.Moxtek_AP3_3));
+            }
          } else if (str.startsWith("Detector thickness:")) {
             double val = Double.parseDouble(str.substring("Detector thickness:".length()).trim());
             mProperties.setNumericProperty(SpectrumProperties.DetectorThickness, val);
@@ -124,8 +149,8 @@ public class BrukerTXT extends BaseSpectrum {
       }
       try {
          int i = 0;
-         for (String str = br.readLine().trim(); (str.length() > 0) && (i < mData.length); str = br.readLine().trim()) {
-            String[] items = str.split("\\h+");
+         for (String str = br.readLine().trim(); (str != null) && (str.length() > 0) && (i < mData.length); str = br.readLine()) {
+            String[] items = str.trim().split("\\h+");
             if (items.length > 1)
                mData[i] = Double.parseDouble(items[1]);
             ++i;

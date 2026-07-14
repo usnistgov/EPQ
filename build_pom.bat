2f1c572002a3f55d52086f03f59c0b274a17939d
Nicholas Ritchie <nritchie@duck-and-cover.com>
2026-07-14 11:35:36 -0400

    Fixing build in JDK 26

--- build_pom.bat
+++ build_pom.bat
@@ -3,4 +3,4 @@ REM Version information
 SET "NUM_VER=15.1.48"
 SET "SED_PATH=C:\Program Files (x86)\GnuWin32\bin"
 
-"%SED_PATH%\sed.exe" -e "s/NUMBER_VERSION/%NUM_VER%/g" pom.template > pom.xml
+"%SED_PATH%\sed.exe" -e "s/NUMBER_VERSION/%NUM_VER%/g" -e "s/JDK_VERSION/%JDK_VER%/g" pom.template > pom.xml || exit /b
